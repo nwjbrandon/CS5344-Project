ffa956be90f51313b99f824f8514b01bd1dc8ef3
nwjbrandon <nwjbrandon@gmail.com>
2024-03-29 17:35:08 +0800

    refactor

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class MovieLens20m:
 
         self.movies_df = self.spark.read.option("header", True).csv(self.movie_fname)
         self.ratings_df = self.spark.read.option("header", True).csv(self.ratings_fname)
+        self.popular_movies_df = self.compute_movie_popularity()
 
     def get_movies_df(self):
         return self.movies_df
@@ -21,17 +22,25 @@ class MovieLens20m:
         popular_movies_df = self.ratings_df.groupBy("movieId").agg(F.count("userId"), F.avg("rating")).withColumnRenamed("count(userId)", "n_ratings").withColumnRenamed("avg(rating)", "avg_rating")
         popular_movies_df = popular_movies_df.join(self.movies_df, popular_movies_df.movieId == self.movies_df.movieId)
         return popular_movies_df
+    
+    def get_number_of_rating_statistics(self):
+        return self.popular_movies_df.select([F.mean('n_ratings'), F.min('n_ratings'), F.max('n_ratings')])
+
+    def get_average_rating_statistics(self):
+        return self.popular_movies_df.select([F.mean('avg_rating'), F.min('avg_rating'), F.max('avg_rating')])
 
     def rank_movies_by_number_of_ratings(self):
-        popular_movies_df = self.compute_movie_popularity().sort(F.desc("n_ratings"))
-        return popular_movies_df
+        popular_movies_by_number_of_ratings_df = self.popular_movies_df.sort(F.desc("n_ratings"))
+        return popular_movies_by_number_of_ratings_df
 
     def rank_movies_by_average_ratings(self):
-        popular_movies_df = self.compute_movie_popularity().sort(F.desc("avg_rating"))
-        return popular_movies_df
+        popular_movies_by_average_ratings_df = self.popular_movies_df.sort(F.desc("avg_rating"))
+        return popular_movies_by_average_ratings_df
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Lab2").getOrCreate()
     movielens20m = MovieLens20m(spark=spark)
+    movielens20m.get_number_of_rating_statistics().show(10)
+    movielens20m.get_average_rating_statistics().show(10)
     movielens20m.rank_movies_by_number_of_ratings().show(10)
     movielens20m.rank_movies_by_average_ratings().show(10)
