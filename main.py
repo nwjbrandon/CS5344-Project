23f7aff9b89d34be85a779751f805d068b1ce66c
nwjbrandon <nwjbrandon@gmail.com>
2024-03-29 18:15:39 +0800

    add percentile calculations

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ class MovieLens20m:
         self.movie_fname = os.path.join(data_dir, "movies.csv")
         self.ratings_fname = os.path.join(data_dir, "ratings.csv")
 
+        # Load datasets
         self.movies_df = self.spark.read.option("header", True).csv(self.movie_fname)
         self.ratings_df = self.spark.read.option("header", True).csv(self.ratings_fname)
+
+        # Compute movie popularity and rating statistics
         self.popular_movies_df = self.compute_movie_popularity()
+        self.n_rating_statistics = self.compute_statistics_for_number_of_rating()
 
     def get_movies_df(self):
         return self.movies_df
@@ -23,21 +27,45 @@ class MovieLens20m:
 
     def compute_movie_popularity(self):
         popular_movies_df = self.ratings_df.groupBy("movieId")\
-            .agg(F.count("userId"), F.avg("rating"), F.stddev("rating"))\
-            .withColumnRenamed("count(userId)", "n_ratings")\
-            .withColumnRenamed("avg(rating)", "avg_rating")\
-            .withColumnRenamed("stddev_samp(rating)", "std_rating")
-        popular_movies_df = popular_movies_df.join(self.movies_df, popular_movies_df.movieId == self.movies_df.movieId)
+            .agg(
+                F.count("userId").alias("n_rating"), 
+                F.avg("rating").alias("avg_rating"), 
+                F.stddev("rating").alias("std_rating")
+            )
+        popular_movies_df = popular_movies_df.join(
+            self.movies_df, popular_movies_df.movieId == self.movies_df.movieId
+        )
         return popular_movies_df
 
-    def get_number_of_rating_statistics(self):
-        return self.popular_movies_df.select([F.mean('n_ratings'), F.min('n_ratings'), F.max('n_ratings')])
-
-    def get_average_rating_statistics(self):
-        return self.popular_movies_df.select([F.mean('avg_rating'), F.min('avg_rating'), F.max('avg_rating')])
-
+    def compute_statistics_for_number_of_rating(self):
+        n_rating_stats = self.popular_movies_df.select([
+            F.mean('n_rating').alias("n_rating_avg"), 
+            F.min('n_rating').alias("n_rating_min"), 
+            F.max('n_rating').alias("n_rating_max")
+        ]).collect()
+        if len(n_rating_stats) == 0:
+            raise "Not Enough Data"
+        
+        n_rating_stats = n_rating_stats[0]
+        n_rating_avg = n_rating_stats.n_rating_avg
+        n_rating_min = n_rating_stats.n_rating_min
+        n_rating_max = n_rating_stats.n_rating_max
+        percentiles = self.popular_movies_df.approxQuantile('n_rating', [0.25, 0.50, 0.75], 0)
+        n_rating_25_percentile, n_rating_50_percentile, n_rating_75_percentile = percentiles
+        return {
+            "n_rating_avg": n_rating_avg,
+            "n_rating_min": n_rating_min,
+            "n_rating_max": n_rating_max,
+            "n_rating_25_percentile": n_rating_25_percentile,
+            "n_rating_50_percentile": n_rating_50_percentile,
+            "n_rating_75_percentile": n_rating_75_percentile,
+        }
+    
+    def get_statistics_for_number_of_rating(self):
+        return self.n_rating_statistics
+    
     def rank_movies_by_number_of_ratings(self):
-        popular_movies_by_number_of_ratings_df = self.popular_movies_df.sort(F.desc("n_ratings"))
+        popular_movies_by_number_of_ratings_df = self.popular_movies_df.sort(F.desc("n_rating"))
         return popular_movies_by_number_of_ratings_df
 
     def rank_movies_by_average_ratings(self):
@@ -45,14 +73,17 @@ class MovieLens20m:
         return popular_movies_by_average_ratings_df
 
     def rank_movies_by_std_ratings(self):
-        popular_movies_by_std_ratings_df = self.popular_movies_df.filter(self.popular_movies_df.std_rating != np.nan).sort(F.desc("std_rating"))
+        popular_movies_by_std_ratings_df = self.popular_movies_df.filter(
+            self.popular_movies_df.std_rating != np.nan).sort(F.desc("std_rating")
+        )
         return popular_movies_by_std_ratings_df
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Lab2").getOrCreate()
+    spark = SparkSession.builder.appName("Project").getOrCreate()
     movielens20m = MovieLens20m(spark=spark)
-    movielens20m.get_number_of_rating_statistics().show(10)
-    movielens20m.get_average_rating_statistics().show(10)
+    n_rating_statistics = movielens20m.get_statistics_for_number_of_rating()
+    print("n_rating_statistics:", n_rating_statistics)
+
     movielens20m.rank_movies_by_number_of_ratings().show(10)
     movielens20m.rank_movies_by_average_ratings().show(10)
     movielens20m.rank_movies_by_std_ratings().show(10)
