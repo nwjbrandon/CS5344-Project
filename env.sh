77f105fc508cb34e3a140d74a59861b083dd0337
aiqqia <aiqqia.ag@gmail.com>
2024-04-01 03:15:38 +0800

    Add the initial recommendation code

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 # docker build -t aiqqia/spark:2.2.1 . -f Dockerfile
-docker run --name ws --rm -v ./:/data -it nwjbrandon/spark:2.2.1 bash
+docker run --name ws --rm -v ./:/data -it aiqqia/spark:2.2.1 bash
