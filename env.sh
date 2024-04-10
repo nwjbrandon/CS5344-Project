edf0d5f11a9c46e9245761e2a9842fc6b327618a
nwjbrandon <nwjbrandon@gmail.com>
2024-04-11 00:16:42 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-docker build -t nwjbrandon/spark:3.0.1 . -f Dockerfile
+# docker build -t nwjbrandon/spark:3.0.1 . -f Dockerfile
 docker run --name ws1 --rm -v ./:/data -it nwjbrandon/spark:3.0.1 bash
